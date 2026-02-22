ac605e39cc53d3346d59d6c7fd46f926e3abeb67
RyuNen344 <s1100633@outlook.com>
2026-02-23 03:17:14 +0900

    feat: enhance Timber integration by adding null checks for log tags and throwable parameters

--- plugin/src/main/kotlin/io/github/ryunen344/log2timber/visitor/Log2TimberMethodVisitor.kt
+++ plugin/src/main/kotlin/io/github/ryunen344/log2timber/visitor/Log2TimberMethodVisitor.kt
@@ -19,6 +19,7 @@
 
 package io.github.ryunen344.log2timber.visitor
 
+import org.objectweb.asm.Label
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 import org.slf4j.Logger
@@ -40,6 +41,7 @@ public class Log2TimberMethodVisitor(
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(javaClass) }
 
+    @Suppress("LongMethod")
     override fun visitMethodInsn(
         opcode: Int,
         owner: String?,
@@ -52,12 +54,22 @@ public class Log2TimberMethodVisitor(
             when (name) {
                 "d", "i", "w", "e", "v", "wtf" -> {
                     when (descriptor) {
-                        // Log.{v,d,i,w,e,wtf}(tag: String, message: String): Int
+                        // Log.{v,d,i,w,e,wtf}(@Nullable tag: String, @NonNull message: String): Int
                         "(Ljava/lang/String;Ljava/lang/String;)I" -> {
+                            val nullLabel = Label()
+                            val endLabel = Label()
+
                             // stack: [tag, message] -> [message, tag]
                             super.visitInsn(Opcodes.SWAP)
 
-                            // call Timber.tag(tag)
+                            // stack: [message, tag] -> [message, tag, tag]
+                            super.visitInsn(Opcodes.DUP)
+
+                            // if tag is null, jump to nullLabel
+                            // stack: [message, tag, tag] -> [message, tag]
+                            super.visitJumpInsn(Opcodes.IFNULL, nullLabel)
+
+                            // if tag is not null, call Timber.tag(tag).{v,d,i,w,e,wtf}(message)
                             super.visitMethodInsn(
                                 Opcodes.INVOKESTATIC,
                                 "timber/log/Timber",
@@ -66,10 +78,11 @@ public class Log2TimberMethodVisitor(
                                 false,
                             )
 
-                            // stack: [message, Timber$Tree]
+                            // stack: [message, Timber$Tree]　-> [Timber$Tree, message]
                             super.visitInsn(Opcodes.SWAP)
 
                             // empty Object[] for varargs
+                            // stack: [Timber$Tree, message] -> [Timber$Tree, message, Object[]]
                             super.visitInsn(Opcodes.ICONST_0)
                             super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
 
@@ -82,16 +95,51 @@ public class Log2TimberMethodVisitor(
                                 false,
                             )
 
+                            // jump to end
+                            super.visitJumpInsn(Opcodes.GOTO, endLabel)
+
+                            // nullLabel: call Timber.{v,d,i,w,e,wtf}(message)
+                            super.visitLabel(nullLabel)
+
+                            // stack: [message, null] -> [message]
+                            super.visitInsn(Opcodes.POP)
+
+                            // empty Object[] for varargs
+                            // stack: [message] -> [message, Object[]]
+                            super.visitInsn(Opcodes.ICONST_0)
+                            super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
+
+                            // call Timber.{v,d,i,w,e,wtf}
+                            super.visitMethodInsn(
+                                Opcodes.INVOKESTATIC,
+                                "timber/log/Timber",
+                                name,
+                                "(Ljava/lang/String;[Ljava/lang/Object;)V",
+                                false,
+                            )
+
+                            super.visitLabel(endLabel)
+
                             // Log.{v,d,i,w,e,wtf} returns int, so push 0 to stack
                             super.visitInsn(Opcodes.ICONST_0)
                         }
 
-                        // Log.{w,wtf}(tag: String, throwable: Throwable): Int
+                        // Log.{w,wtf}(@Nullable tag: String, @Nullable throwable: Throwable): Int
                         "(Ljava/lang/String;Ljava/lang/Throwable;)I" -> {
+                            val nullLabel = Label()
+                            val endLabel = Label()
+
                             // stack: [tag, throwable] -> [throwable, tag]
                             super.visitInsn(Opcodes.SWAP)
 
-                            // call Timber.tag(tag)
+                            // stack: [throwable, tag] -> [throwable, tag, tag]
+                            super.visitInsn(Opcodes.DUP)
+
+                            // if tag is null, jump to nullLabel
+                            // stack: [throwable, tag, tag] -> [throwable, tag]
+                            super.visitJumpInsn(Opcodes.IFNULL, nullLabel)
+
+                            // if tag is not null, call Timber.tag(tag).{w,wtf}(throwable)
                             super.visitMethodInsn(
                                 Opcodes.INVOKESTATIC,
                                 "timber/log/Timber",
@@ -100,10 +148,10 @@ public class Log2TimberMethodVisitor(
                                 false,
                             )
 
-                            // stack: [throwable, Timber$Tree]
+                            // stack: [throwable, Timber$Tree] -> [Timber$Tree, throwable]
                             super.visitInsn(Opcodes.SWAP)
 
-                            // call Timber$Tree.{w,wtf}
+                            // call Timber$Tree.{w,wtf}(throwable)
                             super.visitMethodInsn(
                                 Opcodes.INVOKEVIRTUAL,
                                 $$"timber/log/Timber$Tree",
@@ -112,18 +160,48 @@ public class Log2TimberMethodVisitor(
                                 false,
                             )
 
-                            // Log.{w,wtf} returns void, so push 0 to stack
+                            // jump to end
+                            super.visitJumpInsn(Opcodes.GOTO, endLabel)
+
+                            // nullLabel: call Timber.{w,wtf}(throwable)
+                            super.visitLabel(nullLabel)
+
+                            // stack: [throwable, null] -> [throwable]
+                            super.visitInsn(Opcodes.POP)
+
+                            // call Timber.{w,wtf}
+                            super.visitMethodInsn(
+                                Opcodes.INVOKESTATIC,
+                                "timber/log/Timber",
+                                name,
+                                "(Ljava/lang/Throwable;)V",
+                                false,
+                            )
+
+                            super.visitLabel(endLabel)
+
+                            // Log.{w,wtf} returns int, so push 0 to stack
                             super.visitInsn(Opcodes.ICONST_0)
                         }
 
-                        // Log.{d,i,w,e,v,wtf}(tag: String, message: String, throwable: Throwable): Int
+                        // Log.{v,d,i,w,e,wtf}(@Nullable tag: String, @Nullable message: String, @Nullable throwable: Throwable): Int
                         "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/Throwable;)I" -> {
+                            val nullLabel = Label()
+                            val endLabel = Label()
+
                             // stack: [tag, message, throwable] -> [message, throwable, tag, message, throwable]
                             super.visitInsn(Opcodes.DUP2_X1)
                             // stack: [message, throwable, tag, message, throwable] -> [message, throwable, tag]
                             super.visitInsn(Opcodes.POP2)
 
-                            // call Timber.tag(tag)
+                            // stack: [message, throwable, tag] -> [message, throwable, tag, tag]
+                            super.visitInsn(Opcodes.DUP)
+
+                            // if tag is null, jump to nullLabel
+                            // stack: [message, throwable, tag, tag] -> [message, throwable, tag]
+                            super.visitJumpInsn(Opcodes.IFNULL, nullLabel)
+
+                            // if tag is not null, call Timber.tag(tag).{v,d,i,w,e,wtf}(throwable, message)
                             super.visitMethodInsn(
                                 Opcodes.INVOKESTATIC,
                                 "timber/log/Timber",
@@ -140,10 +218,11 @@ public class Log2TimberMethodVisitor(
                             super.visitInsn(Opcodes.SWAP)
 
                             // empty Object[] for varargs
+                            // stack: [Timber$Tree, throwable, message] -> [Timber$Tree, throwable, message, Object[]]
                             super.visitInsn(Opcodes.ICONST_0)
                             super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
 
-                            // call Timber$Tree.{ d, i, w, e, v, wtf }
+                            // call Timber$Tree.{v,d,i,w,e,wtf}(throwable, message)
                             super.visitMethodInsn(
                                 Opcodes.INVOKEVIRTUAL,
                                 $$"timber/log/Timber$Tree",
@@ -152,7 +231,35 @@ public class Log2TimberMethodVisitor(
                                 false,
                             )
 
-                            // Log.{d,i,w,e,v,wtf} returns int, so push 0 to stack
+                            // jump to end
+                            super.visitJumpInsn(Opcodes.GOTO, endLabel)
+
+                            // nullLabel: call Timber.{v,d,i,w,e,wtf}(throwable, message)
+                            super.visitLabel(nullLabel)
+
+                            // stack: [message, throwable, null] -> [message, throwable]
+                            super.visitInsn(Opcodes.POP)
+
+                            // stack: [message, throwable] -> [throwable, message]
+                            super.visitInsn(Opcodes.SWAP)
+
+                            // empty Object[] for varargs
+                            // stack: [throwable, message] -> [throwable, message, Object[]]
+                            super.visitInsn(Opcodes.ICONST_0)
+                            super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
+
+                            // call Timber.{v,d,i,w,e,wtf}(throwable, message)
+                            super.visitMethodInsn(
+                                Opcodes.INVOKESTATIC,
+                                "timber/log/Timber",
+                                name,
+                                "(Ljava/lang/Throwable;Ljava/lang/String;[Ljava/lang/Object;)V",
+                                false,
+                            )
+
+                            super.visitLabel(endLabel)
+
+                            // Log.{v,d,i,w,e,wtf} returns int, so push 0 to stack
                             super.visitInsn(Opcodes.ICONST_0)
                         }
 
@@ -177,13 +284,23 @@ public class Log2TimberMethodVisitor(
                     }
                 }
 
-                // Log.println(priority: Int, tag: String, message: String): Int
+                // Log.println(priority: Int, @Nullable tag: String, @NonNull message: String): Int
                 // println(ILjava/lang/String;Ljava/lang/String;)I
                 "println" -> {
+                    val nullLabel = Label()
+                    val endLabel = Label()
+
                     // stack: [priority, tag, message] -> [priority, message, tag]
                     super.visitInsn(Opcodes.SWAP)
 
-                    // call Timber.tag(tag)
+                    // stack: [priority, message, tag] -> [priority, message, tag, tag]
+                    super.visitInsn(Opcodes.DUP)
+
+                    // if tag is null, jump to nullLabel
+                    // stack: [priority, message, tag, tag] -> [priority, message, tag]
+                    super.visitJumpInsn(Opcodes.IFNULL, nullLabel)
+
+                    // if tag is not null, call Timber.tag(tag).log(priority, message)
                     super.visitMethodInsn(
                         Opcodes.INVOKESTATIC,
                         "timber/log/Timber",
@@ -198,10 +315,11 @@ public class Log2TimberMethodVisitor(
                     super.visitInsn(Opcodes.POP)
 
                     // empty Object[] for varargs
+                    // stack: [Timber$Tree, priority, message] -> [Timber$Tree, priority, message, Object[]]
                     super.visitInsn(Opcodes.ICONST_0)
                     super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
 
-                    // call Timber$Tree.log
+                    // call Timber$Tree.log(priority, message)
                     super.visitMethodInsn(
                         Opcodes.INVOKEVIRTUAL,
                         $$"timber/log/Timber$Tree",
@@ -209,6 +327,31 @@ public class Log2TimberMethodVisitor(
                         "(ILjava/lang/String;[Ljava/lang/Object;)V",
                         false,
                     )
+
+                    // jump to end
+                    super.visitJumpInsn(Opcodes.GOTO, endLabel)
+
+                    // nullLabel: call Timber.log(priority, message)
+                    super.visitLabel(nullLabel)
+
+                    // stack: [priority, message, null] -> [priority, message]
+                    super.visitInsn(Opcodes.POP)
+
+                    // empty Object[] for varargs
+                    // stack: [priority, message] -> [priority, message, Object[]]
+                    super.visitInsn(Opcodes.ICONST_0)
+                    super.visitTypeInsn(Opcodes.ANEWARRAY, "java/lang/Object")
+
+                    // call Timber.log(priority, message)
+                    super.visitMethodInsn(
+                        Opcodes.INVOKESTATIC,
+                        "timber/log/Timber",
+                        "log",
+                        "(ILjava/lang/String;[Ljava/lang/Object;)V",
+                        false,
+                    )
+
+                    super.visitLabel(endLabel)
 
                     // Log.println returns int, so push 0 to stack
                     super.visitInsn(Opcodes.ICONST_0)
