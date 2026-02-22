50954e5efae5f6e749191225692cc74d5f2d8d68
RyuNen344 <s1100633@outlook.com>
2026-02-22 22:56:22 +0900

    feat: add LogClass for logging functionality with various log methods

---
+++ app-test/src/main/kotlin/io/github/ryunen344/log2timber/app/test/LogClass.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2026 RyuNen344
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package io.github.ryunen344.log2timber.app.test
+
+import android.util.Log
+
+class LogClass {
+
+    fun v() {
+        Log.v(TAG, MESSAGE)
+    }
+
+    fun vTagNull() {
+        Log.v(null, MESSAGE)
+    }
+
+    fun vThrowable() {
+        Log.v(TAG, MESSAGE, throwable)
+    }
+
+    companion object {
+        const val TAG = "LogClass"
+        const val MESSAGE = "LogClass message"
+        val throwable = IllegalStateException("LogClass exception")
+    }
+}
