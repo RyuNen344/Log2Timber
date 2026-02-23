c48c8cfa25020d1e82275190230e331e9d389554
RyuNen344 <s1100633@outlook.com>
2026-02-23 20:18:59 +0900

    test: add LogNestClass and associated logging tests

---
+++ app-test/src/test/kotlin/io/github/ryunen344/log2timber/app/test/nest/LogNestClassErrorTest.kt
@@ -0,0 +1,174 @@
+/*
+ * Copyright (C) 2026 RyuNen344
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package io.github.ryunen344.log2timber.app.test.nest
+
+import android.util.Log
+import assertk.assertThat
+import assertk.assertions.containsOnly
+import assertk.assertions.isEmpty
+import assertk.assertions.isTrue
+import io.github.ryunen344.log2timber.app.test.timber.TestingTree
+import io.github.ryunen344.log2timber.app.test.timber.TimberRecord
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import timber.log.Timber
+
+class LogNestClassErrorTest {
+
+    lateinit var tree: TestingTree
+    lateinit var target: LogNestClass.InnerClass
+
+    @Before
+    fun setup() {
+        tree = TestingTree()
+        Timber.plant(tree)
+        target = LogNestClass.InnerClass()
+    }
+
+    @After
+    fun cleanup() {
+        Timber.uproot(tree)
+    }
+
+    @Test
+    fun test_eIsLoggable() {
+        assertThat(target.eIsLoggable())
+            .isTrue()
+    }
+
+    @Test
+    fun test_eIsLoggableTagNull() {
+        assertThat(target.eIsLoggableTagNull())
+            .isTrue()
+    }
+
+    @Test
+    fun test_e2() {
+        target.e2()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e2TagNull() {
+        target.e2TagNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3() {
+        target.e3()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3MessageNull() {
+        target.e3MessageNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.ERROR,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3ThrowableNull() {
+        target.e3ThrowableNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3TagOnly() {
+        target.e3TagOnly()
+        assertThat(tree.stack).isEmpty()
+    }
+
+    @Test
+    fun test_e3TagNull() {
+        target.e3TagNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3ThrowableOnly() {
+        target.e3ThrowableOnly()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.ERROR,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3MessageOnly() {
+        target.e3MessageOnly()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.ERROR,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_e3Null() {
+        target.e3Null()
+        assertThat(tree.stack).isEmpty()
+    }
+}
---
+++ app-test/src/test/kotlin/io/github/ryunen344/log2timber/app/test/nest/LogNestClassWarnTest.kt
@@ -0,0 +1,206 @@
+/*
+ * Copyright (C) 2026 RyuNen344
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package io.github.ryunen344.log2timber.app.test.nest
+
+import android.util.Log
+import assertk.assertThat
+import assertk.assertions.containsOnly
+import assertk.assertions.isEmpty
+import assertk.assertions.isTrue
+import io.github.ryunen344.log2timber.app.test.timber.TestingTree
+import io.github.ryunen344.log2timber.app.test.timber.TimberRecord
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import timber.log.Timber
+
+class LogNestClassWarnTest {
+
+    lateinit var tree: TestingTree
+    lateinit var target: LogNestClass.InnerClass
+
+    @Before
+    fun setup() {
+        tree = TestingTree()
+        Timber.plant(tree)
+        target = LogNestClass.InnerClass()
+    }
+
+    @After
+    fun cleanup() {
+        Timber.uproot(tree)
+    }
+
+    @Test
+    fun test_wIsLoggable() {
+        assertThat(target.wIsLoggable())
+            .isTrue()
+    }
+
+    @Test
+    fun test_wIsLoggableTagNull() {
+        assertThat(target.wIsLoggableTagNull())
+            .isTrue()
+    }
+
+    @Test
+    fun test_w2() {
+        target.w2()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w2TagNull() {
+        target.w2TagNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w2Throwable() {
+        target.w2Throwable()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w2ThrowableTagNull() {
+        target.w2ThrowableTagNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w2Null() {
+        target.w2Null()
+        assertThat(tree.stack).isEmpty()
+    }
+
+    @Test
+    fun test_w3() {
+        target.w3()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3MessageNull() {
+        target.w3MessageNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3ThrowableNull() {
+        target.w3ThrowableNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3TagOnly() {
+        target.w3TagOnly()
+        assertThat(tree.stack).isEmpty()
+    }
+
+    @Test
+    fun test_w3TagNull() {
+        target.w3TagNull()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3ThrowableOnly() {
+        target.w3ThrowableOnly()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord.exception(
+                Log.WARN,
+                LogNestClass.TAG,
+                null,
+                LogNestClass.throwable,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3MessageOnly() {
+        target.w3MessageOnly()
+        assertThat(tree.stack).containsOnly(
+            TimberRecord(
+                Log.WARN,
+                LogNestClass.TAG,
+                LogNestClass.MESSAGE,
+            ),
+        )
+    }
+
+    @Test
+    fun test_w3Null() {
+        target.w3Null()
+        assertThat(tree.stack).isEmpty()
+    }
+}
